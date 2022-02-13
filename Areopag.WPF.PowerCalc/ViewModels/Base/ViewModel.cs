10e3f6eb8270e28fe450e3b73a8fa90b9cb32a76
Константин Филюрин <rfrsk@yandex.ru>
2022-02-13 22:21:24 +0300

    ViewModel class added

---
+++ Areopag.WPF.PowerCalc/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areopag.WPF.PowerCalc_1.ViewModels.Base
+{
+    internal class ViewModel
+    {
+    }
+}
